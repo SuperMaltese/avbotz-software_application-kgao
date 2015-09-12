f5a4a9ddd75b7d902af6045720f73e243602e95d
SuperMaltese <kevjumba.not@gmail.com>
2015-09-12 16:50:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,64 @@
-//https://www.youtube.com/watch?v=p65XPP53rLo
-//https://www.youtube.com/watch?v=JEpWlTl95Tw
-//https://www.youtube.com/watch?v=UR0hOmjaHp0
 
-void main(){
-  
+#include <iostream>
+#include <vector>
+
+using std::vector;
+vector<int> errors;
+
+int Current_Angle = -45;
+int feedback = 0;
+int error;
+int sum_of_errors = 0;
+
+double K_i = .02;
+double K_d = .0001;
+double K_p = .3;
+double maximum = 127;
+double minimum = -127;
+
+
+
+int main(){
+		Current_Angle = getPitch();
+	  error = getError();
+		feedback = getFeedback();
+		if(error<2 && error>-3)
+            break;
+    }
 }
+
+int8_t getPitch(){
+    Current_Angle += (int)feedback;
+    std::cout<<"angle "<<Current_Angle<<std::endl;
+    return Current_Angle;
+}
+
+int getError(){
+	error = 0 - Current_Angle;
+	std::cout<<"error "<<error<<std::endl;
+	return error;
+}
+
+int getFeedback(){
+    sum_of_errors += error;
+
+    double integral   = sum_of_errors;
+	  double derivative = feedback;
+	  std::cout<<"deriv "<<derivative<<std::endl;
+	  double proportion = error;
+    std::cout<<"proportion "<<proportion<<std::endl;
+	  double feedback = K_p*proportion + K-i*integral + K-d*derivative;
+
+	std::cout<<"integral "<<integral<<std::endl;
+   //     if (feedback<minimum)
+     //       feedback = minimum;
+  //      if (feedback>maximum)
+   //         feedback = maximum;
+  //      std::cout<<"derivative "<<derivative<<std::endl;
+
+        std::cout<<"feedback "<<feedback<<std::endl;
+        std::cout<<""<<std::endl;
+        return (int)feedback;
+
+}
+
